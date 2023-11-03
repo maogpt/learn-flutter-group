24bcc44b014ea850e8786cd7c1718da7b8e36c6e
maogpt <kelikejitech@gmail.com>
2023-11-03 15:11:42 +0800

    feat: Step 3: Implement the button row

--- flutter_layout_demo/lib/main.dart
+++ flutter_layout_demo/lib/main.dart
@@ -2,48 +2,81 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: const Text(
-                'Oeschinen Lake Campground',
-                style:
-                    TextStyle(color: Colors.amber, fontWeight: FontWeight.bold),
-              ),
-            ),
-            const Text('Kandersteg, Switzerland',
-                style: TextStyle(color: Colors.red))
-          ],
-        ),
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      const Text('41')
-    ],
-  ),
-);
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                        color: Colors.amber, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const Text('Kandersteg, Switzerland',
+                    style: TextStyle(color: Colors.red))
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41')
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(children: [titleSection]),
+        body: Column(children: [titleSection, buttonSection]),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
