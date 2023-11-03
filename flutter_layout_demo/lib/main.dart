08b02c1281e88a96b99623f66120b32c4d2b75c2
maogpt <kelikejitech@gmail.com>
2023-11-03 16:13:59 +0800

    Step 5: Implement the image section

--- flutter_layout_demo/lib/main.dart
+++ flutter_layout_demo/lib/main.dart
@@ -67,7 +67,17 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(children: [titleSection, buttonSection, textSection]),
+        body: Column(children: [
+          Image.asset(
+            'images/lake.jpg',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+          titleSection,
+          buttonSection,
+          textSection,
+        ]),
       ),
     );
   }
